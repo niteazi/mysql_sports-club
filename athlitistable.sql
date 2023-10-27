868e6ca010a912af54d0eaec8058290cef2c9481
Nik <130102204+niteazi@users.noreply.github.com>
2023-10-27 20:17:42 +0300

    1

---
+++ athlitistable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `a8lhths` (
+  `kwdikos` int NOT NULL,
+  `onomateponymo` varchar(40) NOT NULL,
+  `etiypiresias` int NOT NULL,
+  `amivi` float NOT NULL,
+  PRIMARY KEY (`kwdikos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
