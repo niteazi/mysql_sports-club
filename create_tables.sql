3f986d6030738320e25731a05627e331fca7f18c
Nik <130102204+niteazi@users.noreply.github.com>
2023-10-28 13:12:33 +0300

    tables

--- create_tables.sql
+++ create_tables.sql
@@ -11,7 +11,17 @@ CREATE TABLE `A8LITIS` (
 CREATE TABLE `PROPONHTHS` (
   `kwdikos` int(7) NOT NULL,
   `onomateponymo` varchar(40) NOT NULL,
-  `etiypiresias` int NOT NULL,
+  `etiypiresias` int(2) NOT NULL,
   `amivi` float NOT NULL,
   PRIMARY KEY (`kwdikos`)
 );
+
+
+CREATE TABLE `PROPONHSH` (
+  `kwdikos` int(4) NOT NULL,
+  `wra dieksagwghs` TIME NOT NULL,
+  `hmera dieksagwghs` VARCHAR(10) NOT NULL,
+  `diarkeia se lepta` int(3) NOT NULL,
+  PRIMARY KEY (`kwdikos`)
+);
+
