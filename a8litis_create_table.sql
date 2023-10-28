f17340f61c0693029633b349fc53e683beaeb1f9
Nik <130102204+niteazi@users.noreply.github.com>
2023-10-28 12:47:07 +0300

    tables

--- a8litis_create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `A8LITIS` (
-  `aem` int(6) NOT NULL,
-  `onomateponymo` varchar(40) NOT NULL,
-  `fylo` int NOT NULL,
-  `hmerominia eggrafis` float NOT NULL,
-  PRIMARY KEY (`aem`)
-);
--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,12 @@
+CREATE TABLE `A8LITIS` (
+  `aem` int(6) NOT NULL,
+  `onomateponymo` varchar(40) NOT NULL,
+  `fylo` int NOT NULL,
+  `hmerominia eggrafis` float NOT NULL,
+  PRIMARY KEY (`aem`)
+);
+
+
 CREATE TABLE `PROPONHTHS` (
   `kwdikos` int(7) NOT NULL,
   `onomateponymo` varchar(40) NOT NULL,
