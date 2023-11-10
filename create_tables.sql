d0224f6fbd08ea8aaaad07f27560abcc43f3a579
Nik <130102204+niteazi@users.noreply.github.com>
2023-11-10 02:59:01 +0200

    .

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `PROPONHTHS` (
+CREATE TABLE if not exists `PROPONHTHS` (
   `kwdikos` int(7) NOT NULL,
   `onomateponymo` varchar(40) NOT NULL,
   `etiypiresias` int(2) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE `PROPONHTHS` (
   PRIMARY KEY (`kwdikos`)
 );
 
-CREATE TABLE `A8LITIS` (
+CREATE TABLE if not exists `A8LITIS` (
   `aem` int(6) NOT NULL PRIMARY KEY,
   `onomateponymo` varchar(40) NOT NULL,
   `fylo` varchar(10) NOT NULL CHECK (fylo in ('Andras', 'Gynaika')),
@@ -18,7 +18,7 @@ CREATE TABLE `A8LITIS` (
 
 
 
-CREATE TABLE `PROPONHSH` (
+CREATE TABLE if not exists `PROPONHSH` (
   `kwdikos` int(4) NOT NULL,
   `wra_dieksagwghs` TIME NOT NULL,
   `hmera_dieksagwghs` VARCHAR(10) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `PROPONHSH` (
 );
 
 
-CREATE TABLE `PROPONHSH_PROPONHTHS` (
+CREATE TABLE if not exists `PROPONHSH_PROPONHTHS` (
   `proponhsh_kwdikos` int(4) NOT NULL,
   `proponhtes_kwdikos` int(7) NOT NULL,
   PRIMARY KEY (`proponhsh_kwdikos`, `proponhtes_kwdikos`),
