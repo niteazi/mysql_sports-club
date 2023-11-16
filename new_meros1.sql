ef65a2f75b455f6a4b4a732b62858a568ec84e33
Nik <130102204+niteazi@users.noreply.github.com>
2023-11-16 19:40:01 +0200

    .

--- new_meros1.sql
+++ new_meros1.sql
@@ -4,7 +4,7 @@ CREATE DATABASE if NOT EXISTS sports_club;
 --drop TABLES
 DROP TABLE IF EXISTS `EGKATASTASEIS`;
 DROP TABLE IF EXISTS `SEASON`;
-DROP TABLE IF EXISTS `ATHLIMA`;
+DROP TABLE IF EXISTS `A8LIMA`;
 DROP TABLE IF EXISTS `A8LITIS`;
 DROP TABLE IF EXISTS `PROPONHSEIS_ATHLITI`;
 
@@ -18,17 +18,17 @@ CREATE TABLE IF NOT EXISTS `EGKATASTASEIS` (
 );
 
 CREATE TABLE IF NOT EXISTS `SEASON` (
-  `ID_season` int(6) NOT NULL,
+  `epoxh` VARCHAR(20) NOT NULL,
   `etos` int(4) NOT NULL,
   `synolo_agwnwn` int(4) NOT NULL,
   `synolo_nikwn` int(3) NOT NULL,
   `synolo_httwn` int(3) NOT NULL,
-  PRIMARY KEY (`ID_season`)
+  PRIMARY KEY (epoxh, etos)
 );
 
-CREATE TABLE IF NOT EXISTS `ATHLIMA` (
+CREATE TABLE IF NOT EXISTS `A8LIMA` (
   `onoma_athlimatos`  varchar(25) NOT NULL,
-   `tropos_paixnidiou` varchar(10) NOT NULL CHECK (tropos_paixnidiou in ('omadiko', 'atomiko')),
+   `tropos_paixnidiou` ENUM('omadiko', 'atomiko') NOT NULL ,
   `exoplismos` int(4),
   `kwdikos_egkatastashs` int(3) NOT NULL,
   PRIMARY KEY (`onoma_athlimatos`),
@@ -48,15 +48,28 @@ CREATE TABLE if not exists `PROPONHTHS` (
 CREATE TABLE IF NOT EXISTS `A8LITIS` (
   `aem_athliti` INT(6) NOT NULL,
   `onomateponymo` VARCHAR(40) NOT NULL,
-  `fylo` VARCHAR(10) NOT NULL CHECK (fylo IN ('Andras', 'Gynaika')),
+  `fylo` ENUM('Andras', 'Gynaika') NOT NULL,
   `hmerominia_eggrafis` DATE NOT NULL,
   `hmerominia_gennhshs` DATE NOT NULL,
   `kwdikos_proponhth` INT(7) NOT NULL,
   `athlima` VARCHAR(25) NOT NULL,
   FOREIGN KEY (`kwdikos_proponhth`) REFERENCES `PROPONHTHS`(`kwdikos_proponhth`),
-  FOREIGN KEY (`athlima`) REFERENCES `ATHLIMA`(`onoma_athlimatos`),
+  FOREIGN KEY (`athlima`) REFERENCES `A8LIMA`(`onoma_athlimatos`),
   PRIMARY KEY (`aem_athliti`)
 );
+CREATE TABLE IF NOT EXISTS `AGWNAS` (
+  `kwdikos_agwna` INT(4) NOT NULL,
+  `epoxh_season` VARCHAR(20) NOT NULL,
+  `etos_season` INT(4) NOT NULL,
+  `wra_dieksagwghs` TIME NOT NULL,
+  `hmeromhnia_dieksagwghs` DATE NOT NULL,
+  `apotelesma` VARCHAR(255) NOT NULL,
+  `eidos_match` ENUM('filiko', 'antagwnistiko') NOT NULL,
+  `topothesia` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`kwdikos_agwna`),
+  FOREIGN KEY (`epoxh_season`, `etos_season`) REFERENCES `SEASON` (`epoxh`, `etos`)
+);
+
 
 CREATE TABLE if not exists `PROPONHSH` (
   `kwdikos_proponhshs` int(4) NOT NULL,
@@ -72,4 +85,12 @@ CREATE TABLE if not exists `PROPONHSEIS_ATHLITI` (
   PRIMARY KEY (`aem_athliti`, `kwdikos_proponhshs`),
   FOREIGN KEY (`aem_athliti`) REFERENCES `A8LITIS`(`aem_athliti`),
   FOREIGN KEY (`kwdikos_proponhshs`) REFERENCES `PROPONHSH`(`kwdikos_proponhshs`)
+);
+
+CREATE TABLE if not exists `AGWNES_ATHLITI` (
+  `aem_athliti` int(6) NOT NULL,
+  `kwdikos_agwna` int(4) NOT NULL,
+  PRIMARY KEY (`aem_athliti`, `kwdikos_agwna`),
+  FOREIGN KEY (`aem_athliti`) REFERENCES `A8LITIS`(`aem_athliti`),
+  FOREIGN KEY (`kwdikos_agwna`) REFERENCES `AGWNAS`(`kwdikos_agwna`)
 );
