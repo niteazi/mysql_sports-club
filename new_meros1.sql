825f76178bf639f52e92d959e14d1ac31a066fd5
Nik <130102204+niteazi@users.noreply.github.com>
2023-11-16 16:44:02 +0200

    .

--- new_meros1.sql
+++ new_meros1.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS sports_club;
 CREATE DATABASE if NOT EXISTS sports_club;
 
 --drop TABLE
@@ -9,37 +10,18 @@ DROP TABLE IF EXISTS `proponhths`;
 
 --CREATE TABLES
 
-CREATE TABLE IF NOT EXISTS `PROPONHTHS` (
-  `kwdikos` int(7) NOT NULL,
-  `onomateponymo` varchar(40) NOT NULL,
-  `etiypiresias` int(2) NOT NULL,
-  `amivi` float NOT NULL,
+CREATE TABLE IF NOT EXISTS `EGKATASTASEIS` (
+  `kwdikos` int(3) NOT NULL,
+  `typos_gypedou` varchar(25) NOT NULL,
+  `xwrhtikothta`  int NOT NULL,
   PRIMARY KEY (`kwdikos`)
 );
 
-CREATE TABLE IF NOT EXISTS `A8LITIS` (
-  `aem` int(6) NOT NULL PRIMARY KEY,
-  `onomateponymo` varchar(40) NOT NULL,
-  `fylo` varchar(10) NOT NULL CHECK (fylo in ('Andras', 'Gynaika')),
-  `hmerominia_eggrafis` date NOT NULL,
-  `kwdikos` int(7) NOT NULL,
-  FOREIGN KEY (`kwdikos`) REFERENCES PROPONHTHS(`kwdikos`)
-);
-
-
-CREATE TABLE IF NOT EXISTS `PROPONHSH` (
-  `kwdikos` int(4) NOT NULL,
-  `wra_dieksagwghs` TIME NOT NULL,
-  `hmera_dieksagwghs` VARCHAR(10) NOT NULL,
-  `diarkeia_se_lepta` int(3) NOT NULL,
-  PRIMARY KEY (`kwdikos`)
-);
-
-
-CREATE TABLE IF NOT EXISTS `PROPONHSH_PROPONHTHS` (
-  `proponhsh_kwdikos` int(4) NOT NULL,
-  `proponhtes_kwdikos` int(7) NOT NULL,
-  PRIMARY KEY (`proponhsh_kwdikos`, `proponhtes_kwdikos`),
-  FOREIGN KEY (`proponhsh_kwdikos`) REFERENCES `PROPONHSH`(`kwdikos`),
-  FOREIGN KEY (`proponhtes_kwdikos`) REFERENCES `PROPONHTHS`(`kwdikos`)
+CREATE TABLE IF NOT EXISTS `SEASON` (
+  `ID` int(6) NOT NULL,
+  `etos` int(4) NOT NULL,
+  `synolo_agwnwn` int(4) NOT NULL,
+  `synolo_nikwn` int(3) NOT NULL,
+  `synolo_httwn` int(3) NOT NULL,
+  PRIMARY KEY (`ID`)
 );
