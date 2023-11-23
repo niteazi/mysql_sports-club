d66516857fadebbb59daf8b8ffe6832a18f233fc
Nik <130102204+niteazi@users.noreply.github.com>
2023-11-23 16:07:51 +0200

    .

--- meros_1.sql
+++ meros_1.sql
@@ -231,4 +231,4 @@ INSERT INTO STATISTIKA
 VALUES 
 (1234,  90, 50, 'hlios','Asteras Korinthou'),
 (2434,  120, 100,NULL,'Aikitoi Xanthis' ),
-(3344, 60, 42, 'psixala', 'Giannis');
+(3344, 60, 42, 'psixala', 'Giannis Papadopoulos');
