0924ee9884a0479034e5c8712ba6c6498482afaa
Nik <130102204+niteazi@users.noreply.github.com>
2023-12-26 07:04:16 +0200

    ,

--- meros_1.sql
+++ meros_1.sql
@@ -63,7 +63,7 @@ CREATE TABLE if not exists `PROPONHTHS` (
 CREATE TABLE IF NOT EXISTS `A8LITIS` (
   `aem_athliti` INT(6) NOT NULL CHECK(aem_athliti BETWEEN 100000 AND 999999),
   `onomateponymo` VARCHAR(40) NOT NULL,
-  `fylo` VARCHAR(10) NOT NULL CHECK(fylo IN ('Andras', 'Gynaika')),
+  `fylo` VARCHAR(10) NOT NULL CHECK(fylo IN ('Άνδρας', 'Γυναίκα')),
   `hmerominia_eggrafis` DATE NOT NULL,
   `hmerominia_gennhshs` DATE NOT NULL,
   `kwdikos_proponhth` VARCHAR(7) NOT NULL CHECK(LENGTH(kwdikos_proponhth) = 7),
@@ -79,8 +79,8 @@ CREATE TABLE IF NOT EXISTS `AGWNAS` (
   `hmeromhnia_dieksagwghs` DATE NOT NULL,
   `wra_dieksagwghs` TIME NOT NULL,
   `apotelesma` VARCHAR(40) NOT NULL,
-  `eidos_match` VARCHAR(20) NOT NULL CHECK(eidos_match IN ('filiko', 'agwnistiko')),
-  `topothesia` VARCHAR(20) NOT NULL CHECK(topothesia IN ('edra','ektos edras')),
+  `eidos_match` VARCHAR(20) NOT NULL CHECK(eidos_match IN ('φιλικό', 'αγωνιστικό')),
+  `topothesia` VARCHAR(20) NOT NULL CHECK(topothesia IN ('έδρα','εκτός έδρας')),
   PRIMARY KEY (`kwdikos_agwna`),
   FOREIGN KEY (`epoxh`, `etos`) REFERENCES `SEASON` (`epoxh`, `etos`),
   FOREIGN KEY (`athlima`) REFERENCES `A8LIMA`(`onoma_athlimatos`)
@@ -142,91 +142,91 @@ CREATE TABLE IF NOT EXISTS `STATISTIKA` (
 --INSERT VALUES
 INSERT INTO A8LIMA
 VALUES 
-('Podosfairo', 'omadiko', 'mpala'),
-('Basket', 'omadiko', 'mpala'),
-('Tennis', 'atomiko', 'raketes'),
-('Volley', 'omadiko', 'mpala'),
-('Kolymvish', 'atomiko',NULL);
+('Ποδόσφαιρο', 'omadiko', 'mpala'),
+('Μπάσκετ', 'omadiko', 'mpala'),
+('Τέννις', 'atomiko', 'raketes'),
+('Βόλευ', 'omadiko', 'mpala'),
+('Κολύμβηση', 'atomiko',NULL);
 
 
 INSERT INTO EGKATASTASEIS
 VALUES 
-(123,'ghpedo podosfairou 5x5','Podosfairo', 'kleisto', 200),
-(124,'ghpedo podosfairou 3x3','Podosfairo', 'kleisto', 180),
-(125,'ghpedo podosfairou 5x5','Podosfairo', 'anoixto', 220),
-(128,'ghpedo basket','Basket', 'kleisto', 200),
-(127,'ghpedo basket','Basket', 'anoixto', 210),
-(173,'ghpedo tennis', 'Tennis','anoixto', 110),
-(174,'ghpedo volley', 'Volley','anoixto', 120),
-(176,'pisina kolymvishs', 'Kolymvish','kleisto', 80),
-(175,'pisina kolymvishs', 'Kolymvish','anoixto', 100);
+(123,'ghpedo Ποδόσφαιροu 5x5','Ποδόσφαιρο', 'kleisto', 200),
+(124,'ghpedo Ποδόσφαιροu 3x3','Ποδόσφαιρο', 'kleisto', 180),
+(125,'ghpedo Ποδόσφαιροu 5x5','Ποδόσφαιρο', 'anoixto', 220),
+(128,'ghpedo Μπάσκετ','Μπάσκετ', 'kleisto', 200),
+(127,'ghpedo Μπάσκετ','Μπάσκετ', 'anoixto', 210),
+(173,'ghpedo Τέννις', 'Τέννις','anoixto', 110),
+(174,'ghpedo Βόλευ', 'Βόλευ','anoixto', 120),
+(176,'pisina Κολύμβησηs', 'Κολύμβηση','kleisto', 80),
+(175,'pisina Κολύμβησηs', 'Κολύμβηση','anoixto', 100);
 
 INSERT INTO SEASON
 VALUES 
-('Xeimerino',2023, 20, 10,10),
-('Earino',2022, 20, 15,5),
-('Xeimerino',2022, 15, 10,5),
-('Earino',2021, 10, 5,5),
-('Xeimerino',2021, 11, 5,6);
+('Χειμερινό',2023, 20, 10,10),
+('Εαρινό',2022, 20, 15,5),
+('Χειμερινό',2022, 15, 10,5),
+('Εαρινό',2021, 10, 5,5),
+('Χειμερινό',2021, 11, 5,6);
 
 
 INSERT INTO PROPONHTHS
 VALUES
-(1234567, 'Podosfairo', 'Georgios Gkolios', 2, 1000),
-(1234568, 'Basket', 'Ioannis Kapis', 3, 1100),
-(1234569, 'Tennis', 'Xrhstos Dimitriadhs', 1, 900),
-(1235469, 'Volley', 'Emanouil Emanouilidis', 2, 800),
-(2359487, 'Kolymvish', 'Isidoros Panagiotou', 5, 1200); 
+(1234567, 'Ποδόσφαιρο', 'Georgios Gkolios', 2, 1000),
+(1234568, 'Μπάσκετ', 'Ioannis Kapis', 3, 1100),
+(1234569, 'Τέννις', 'Xrhstos Dimitriadhs', 1, 900),
+(1235469, 'Βόλευ', 'Emanouil Emanouilidis', 2, 800),
+(2359487, 'Κολύμβηση', 'Isidoros Panagiotou', 5, 1200); 
 
 INSERT INTO A8LITIS
 VALUES 
-(123456, 'Dimitrios Skourths', 'Andras', '2022-09-01', '2000-01-01', 1234567, 'Podosfairo'),
-(133456, 'Trifwnas Anastasiadhs', 'Andras', '2022-09-14', '2000-07-07', 1234567, 'Podosfairo'),
-(123457, 'Orfeas Dedes', 'Andras', '2021-09-05', '2001-05-12', 1234568, 'Basket'),
-(123458, 'Iosif Tsannis', 'Andras', '2021-10-10', '2000-03-15', 1234569, 'Tennis'),
-(123459, 'Maria Papadopoulou', 'Gynaika', '2021-09-13', '2003-01-20', 1234567, 'Podosfairo'),
-(123460, 'Eleni Papadopoulou', 'Gynaika', '2021-09-13', '2003-01-20', 1234568, 'Basket'),
-(123461, 'Euaggelia Kritsotaki', 'Gynaika', '2022-11-16', '2005-04-16', 1234569, 'Tennis'),
-(153456, 'Athina Matziouni', 'Gynaika', '2021-10-01','2001-10-06',1235469,'Volley'),
-(195842, 'Frideriki Papadofasomanolaki','Gynaika','2021-10-03','2008-11-13',2359487,'Kolymvish'),
-(123462, 'Giannis Athanasopoulos', 'Andras', '2022-10-20', '2002-02-25', 1234567, 'Podosfairo'),
-(123463, 'Maria Koutoukou', 'Gynaika', '2022-09-05', '2003-03-10', 1234567, 'Podosfairo'),
-(123464, 'Nikos Papadopoulos', 'Andras', '2022-09-10', '2001-08-15', 1234568, 'Basket'),
-(123465, 'Eirini Georgiou', 'Gynaika', '2022-08-25', '2002-05-20', 1234568, 'Basket'),
-(123466, 'Alexandros Karagiannis', 'Andras', '2022-07-15', '2000-11-30', 1234569, 'Tennis'),
-(123467, 'Sophia Antoniou', 'Gynaika', '2022-10-30', '2001-04-05', 1234569, 'Tennis'),
-(153457, 'Dimitra Karavasili', 'Gynaika', '2021-09-05', '2002-09-15', 1235469, 'Volley'),
-(153458, 'Panagiotis Koutsopoulos', 'Andras', '2022-12-05', '2000-10-10', 1235469, 'Volley'),
-(195843, 'Konstantinos Papadopoulos', 'Andras', '2023-01-10', '2009-02-18', 2359487, 'Kolymvish'),
-(195844, 'Eleni Mitsou', 'Gynaika', '2023-02-15', '2007-07-23', 2359487, 'Kolymvish');
+(123456, 'Δημήτριος Σκούρτης', 'Άνδρας', '2022-09-01', '2000-01-01', 1234567, 'Ποδόσφαιρο'),
+(133456, 'Τρίφωνας Αναστασιάδης', 'Άνδρας', '2022-09-14', '2000-07-07', 1234567, 'Ποδόσφαιρο'),
+(123457, 'Ορφέας Δέδες', 'Άνδρας', '2021-09-05', '2001-05-12', 1234568, 'Μπάσκετ'),
+(123458, 'Ιωσίφ Τσάννης', 'Άνδρας', '2021-10-10', '2000-03-15', 1234569, 'Τέννις'),
+(123459, 'Μαρία Παπαδοπούλου', 'Γυναίκα', '2021-09-13', '2003-01-20', 1234567, 'Ποδόσφαιρο'),
+(123460, 'Ελένη Παπαδοπούλου', 'Γυναίκα', '2021-09-13', '2003-01-20', 1234568, 'Μπάσκετ'),
+(123461, 'Ευαγγελία Κριτσοτάκη', 'Γυναίκα', '2022-11-16', '2005-04-16', 1234569, 'Τέννις'),
+(153456, 'Αθηνά Ματζιούνη', 'Γυναίκα', '2021-10-01','2001-10-06',1235469,'Βόλευ'),
+(195842, 'Φριδερίκη Παπαδοφασομανολάκη','Γυναίκα','2021-10-03','2008-11-13',2359487,'Κολύμβηση'),
+(123462, 'Ιωάννης Γερασιμόπουλος', 'Άνδρας', '2022-10-20', '2002-02-25', 1234567, 'Ποδόσφαιρο'),
+(123463, 'Μαρία Κουτούκου', 'Γυναίκα', '2022-09-05', '2003-03-10', 1234567, 'Ποδόσφαιρο'),
+(123464, 'Νικόλαος Παπαδόπουλος', 'Άνδρας', '2022-09-10', '2001-08-15', 1234568, 'Μπάσκετ'),
+(123465, 'Ειρήνη Γεωργίου', 'Γυναίκα', '2022-08-25', '2002-05-20', 1234568, 'Μπάσκετ'),
+(123466, 'Αλέξανδρος Καραγιάννης', 'Άνδρας', '2022-07-15', '2000-11-30', 1234569, 'Τέννις'),
+(123467, 'Σοφία Αντωνίου', 'Γυναίκα', '2022-10-30', '2001-04-05', 1234569, 'Τέννις'),
+(153457, 'Δήμητρα Καραβασίλη', 'Γυναίκα', '2021-09-05', '2002-09-15', 1235469, 'Βόλευ'),
+(153458, 'Παναγιώτης Κουτσόπουλος', 'Άνδρας', '2022-12-05', '2000-10-10', 1235469, 'Βόλευ'),
+(195843, 'Σίσιφος Ποσειδώνας Σμιθ', 'Άνδρας', '2023-01-10', '2009-02-18', 2359487, 'Κολύμβηση'),
+(195844, 'Ελένη Μίτσου', 'Γυναίκα', '2023-02-15', '2007-07-23', 2359487, 'Κολύμβηση');
 
 INSERT INTO AGWNAS
 VALUES 
-(1234, 'Podosfairo','Xeimerino', 2023, '2023-05-01', '15:00:00', 'nikh', 'filiko', 'edra'),
-(2434,'Basket','Xeimerino', 2022, '2022-10-12', '17:00:00', 'htta', 'filiko', 'edra'),
-(3344, 'Tennis','Earino', 2022, '2022-03-23', '12:00:00', 'nikh', 'agwnistiko', 'ektos edras'),
-(3476, 'Volley','Xeimerino', 2021, '2021-11-01', '18:00:00', 'nikh', 'filiko', 'edra'),
-(3477, 'Kolymvish','Xeimerino', 2022, '2022-10-01', '11:30:00', 'nikh', 'filiko', 'edra'),
-(3478, 'Podosfairo','Earino', 2022, '2022-05-14', '16:00:00', 'htta', 'agwnistiko', 'ektos edras'),
-(3479, 'Basket','Earino', 2022, '2022-05-02', '17:00:00', 'nikh', 'filiko', 'edra'),
-(3480, 'Tennis','Earino', 2022, '2022-04-03', '18:00:00', 'htta', 'agwnistiko', 'edra'),
-(3481, 'Volley','Xeimerino', 2021, '2021-02-04', '19:00:00', 'nikh', 'filiko', 'edra'),
-(3482, 'Kolymvish','Xeimerino', 2023, '2023-11-24', '20:00:00', 'htta', 'agwnistiko', 'ektos edras'),
-(3483, 'Podosfairo','Earino', 2022, '2022-06-06', '14:00:00', 'nikh', 'filiko', 'edra'),
-(3484, 'Basket','Earino', 2021, '2023-07-07', '16:30:00', 'htta', 'agwnistiko', 'ektos edras'),
-(3485, 'Tennis','Xeimerino', 2023, '2023-12-08', '10:00:00', 'nikh', 'filiko', 'edra');
+(1234, 'Ποδόσφαιρο','Χειμερινό', 2023, '2023-05-01', '15:00:00', 'νίκη', 'φιλικό', 'έδρα'),
+(2434,'Μπάσκετ','Χειμερινό', 2022, '2022-10-12', '17:00:00', 'ήττα', 'φιλικό', 'έδρα'),
+(3344, 'Τέννις','Εαρινό', 2022, '2022-03-23', '12:00:00', 'νίκη', 'αγωνιστικό', 'εκτός έδρας'),
+(3476, 'Βόλευ','Χειμερινό', 2021, '2021-11-01', '18:00:00', 'νίκη', 'φιλικό', 'έδρα'),
+(3477, 'Κολύμβηση','Χειμερινό', 2022, '2022-10-01', '11:30:00', 'νίκη', 'φιλικό', 'έδρα'),
+(3478, 'Ποδόσφαιρο','Εαρινό', 2022, '2022-05-14', '16:00:00', 'ήττα', 'αγωνιστικό', 'εκτός έδρας'),
+(3479, 'Μπάσκετ','Εαρινό', 2022, '2022-05-02', '17:00:00', 'νίκη', 'φιλικό', 'έδρα'),
+(3480, 'Τέννις','Εαρινό', 2022, '2022-04-03', '18:00:00', 'ήττα', 'αγωνιστικό', 'έδρα'),
+(3481, 'Βόλευ','Χειμερινό', 2021, '2021-02-04', '19:00:00', 'νίκη', 'φιλικό', 'έδρα'),
+(3482, 'Κολύμβηση','Χειμερινό', 2023, '2023-11-24', '20:00:00', 'ήττα', 'αγωνιστικό', 'εκτός έδρας'),
+(3483, 'Ποδόσφαιρο','Εαρινό', 2022, '2022-06-06', '14:00:00', 'νίκη', 'φιλικό', 'έδρα'),
+(3484, 'Μπάσκετ','Εαρινό', 2021, '2023-07-07', '16:30:00', 'ήττα', 'αγωνιστικό', 'εκτός έδρας'),
+(3485, 'Τέννις','Χειμερινό', 2023, '2023-12-08', '10:00:00', 'νίκη', 'φιλικό', 'έδρα');
 
 INSERT INTO PROPONHSH
 VALUES 
-(1001, '19:00:00', 'Tetarti', 60),
-(2002, '16:30:00', 'Paraskeui', 90),
-(3003, '14:50:00', 'Triti', 120),
-(4004, '16:00:00', 'Deutera', 90),
-(4059, '17:30:00', 'Deutera', 90),
-(5005, '18:00:00', 'Pempti', 60),
-(6006, '11:00:00', 'Savvato', 120),
-(7007, '12:00:00', 'Savvato', 90),
-(8008, '15:00:00', 'Tetarti', 120);
+(1001, '19:00:00', 'Τετάρτη', 60),
+(2002, '16:30:00', 'Παρασκευή', 90),
+(3003, '14:50:00', 'Τρίτη', 120),
+(4004, '16:00:00', 'Δευτέρα', 90),
+(4059, '17:30:00', 'Δευτέρα', 90),
+(5005, '18:00:00', 'Πέμπτη', 60),
+(6006, '11:00:00', 'Σάββατο', 120),
+(7007, '12:00:00', 'Σάββατο', 90),
+(8008, '15:00:00', 'Τετάρτη', 120);
 
 INSERT INTO AGWNES_A8LITI 
 VALUES 
